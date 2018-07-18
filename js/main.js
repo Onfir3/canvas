1ae6984c2a537629c075b52e8616ea86cc8d6bdc
Onfir3 <w526529650@outlook.com>
2018-07-18 17:28:27 +0800

    完善canvas功能

--- js/main.js
+++ js/main.js
@@ -1,20 +1,67 @@
 var yyy = document.getElementById('xxx');
 var context = yyy.getContext('2d');
+var lineWidth = 5
 
 autoSetCanvasSize(yyy)
 
 listenToUser(yyy)
 
 var eraserEnabled = false
-eraser.onclick = function () {
-    eraserEnabled = true
-    actions.className = "actions x"
-}
-brush.onclick = function () {
+pen.onclick = function(){   
     eraserEnabled = false
-    actions.className = "actions"
+    pen.classList.add('active')
+    eraser.classList.remove('active')
+}
+eraser.onclick = function(){
+    eraserEnabled = true
+    eraser.classList.add('active')
+    pen.classList.remove('active')
+}
+clear.onclick = function(){
+    context.clearRect(0,0,yyy.width,yyy.height)
+}
+download.onclick = function(){
+    var url = yyy.toDataURL("image/png")
+    console.log(url)
+    var a = document.createElement('a')
+    document.body.appendChild(a)
+    a.href = url 
+    a.download = 'xxxx'
+    a.click()
 }
 
+red.onclick = function(){
+    context.fillStyle = 'red'  
+    context.strokeStyle = 'red' 
+    red.classList.add('active')
+    green.classList.remove('active')
+    blue.classList.remove('active')
+}
+green.onclick = function(){
+    context.fillStyle = 'green'  
+    context.strokeStyle = 'green'  
+    green.classList.add('active')
+    red.classList.remove('active')
+    blue.classList.remove('active')
+}
+blue.onclick = function(){
+    context.fillStyle = 'blue'  
+    context.strokeStyle = 'blue'
+    blue.classList.add('active') 
+    green.classList.remove('active') 
+    red.classList.remove('active')
+}
+
+thin.onclick = function(){
+    lineWidth = 5
+    thin.classList.add('active')
+    thick.classList.remove('active')
+}
+thick.onclick = function(){
+    lineWidth = 10
+    thick.classList.add('active')
+    thin.classList.remove('active')
+}
 /************/
 function autoSetCanvasSize(canvas) {
     setCanvasSize()
@@ -31,16 +78,14 @@ function autoSetCanvasSize(canvas) {
 }
 function drawCircle(x, y, radius) {
     context.beginPath()
-    context.fillStyle = "blue"
     context.arc(x, y, radius, 0, Math.PI * 2)
     context.fill()
 }
 
 function drawLine(x1, y1, x2, y2) {
     context.beginPath()
-    context.strokeStyle = "blue"
     context.moveTo(x1, y1)
-    context.lineWidth = 5
+    context.lineWidth = lineWidth
     context.lineTo(x2, y2)
     context.stroke()
     context.closePath()
